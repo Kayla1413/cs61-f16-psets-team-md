df5e855249e6f8f803b3c5bd3cca11637106605e
vagrant <vagrant@CS61-Appliance>
2016-09-17 22:58:01 +0000

    sync

--- pset1/m61.c
+++ pset1/m61.c
@@ -8,20 +8,16 @@
 
 // push on 9-17 to bry
 
+/* variables from m61.h for getstats */
 static unsigned long long nactive, active_size, ntotal, total_size, nfail, fail_size;
-char* heap_min, heap_max;
+char* heap_min;
+char* heap_max;
 
-
-/* per-allocation metadata */
+/* metadata structure to get active_size */
 typedef struct {
     size_t block_size;
-    /* payload */
     const char* file;
     int line;
-    /* padding */
-    	// Not Applicable Yet
-    /* Temp Address Holder */
-    char *address;
 } metadata;
 metadata main_metadata; 
 
@@ -34,50 +30,39 @@ metadata main_metadata;
 void* m61_malloc(size_t sz, const char* file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     // Your code here.
-	if (sz > SIZE_MAX - sizeof(struct m61_statistics)) {
-        nfail++;
-        fail_size += sz;
-        return NULL;
-    }
-
-    // Checking to see if conditions are met & defining Null pointer
-    
     void *ptr = NULL;
-    ptr = base_malloc(sz);  
-    /* base malloc failed */
+    /* allocating more space than the user requested for metadata */
+    ptr = base_malloc(sz+sizeof(metadata));  
+    /* if base_malloc fails or succeeds */
     if (!ptr){
 	nfail++;
 	fail_size+=sz;
-	return NULL;	
+	return ptr;	
     }
     else{
-	/* get statistics */
+	/* updating statistics */
 	ntotal++;
 	total_size+=sz;
 	nactive++;
         active_size+=sz;    
-	/* initialize metadata */
+	/* initializing metadata */
+	metadata *main_metadata = (metadata*) ptr;
+	main_metadata->block_size=sz;
+	main_metadata->file=file;
+	main_metadata->line=line;
+	/* updating more statistics */
+	/*
+	if(heap_max==NULL || heap_max<ptr)
+		heap_max=ptr;
 
-	main_metadata.block_size=sz;
-	main_metadata.file=file;
-	main_metadata.line=line;
-
-	/* setting up min heap stuff to check min address - doesn't work 
-		int ptr2 = base_malloc(sz);
-		int value = strcmp(&ptr2, main_metadata.address);
-		if(value < 0){
-			heap_min = (long long) &ptr2;
-		//memcpy(&heap_min, &ptr2, sizeof(unsigned long long );
-		}
-		main_metadata.address = &ptr2;
-		return ptr2;
+	return ptr;
 	*/
-	return base_malloc(sz);
+	// return ptr + sizeof(metadata);
     }
 }
 
 
-/// m61_free(ptr, file, line)
+///    m61_free(ptr, file, line)
 ///    Free the memory space pointed to by `ptr`, which must have been
 ///    returned by a previous call to m61_malloc and friends. If
 ///    `ptr == NULL`, does nothing. The free was called at location
@@ -86,20 +71,15 @@ void* m61_malloc(size_t sz, const char* file, int line) {
 void m61_free(void *ptr, const char *file, int line) {
     (void) file, (void) line;   // avoid uninitialized variable warnings
     // Your code here.
-
-    // active_size isn't defined right, I just free 3x the amt of ptr size for now
-    // active_size-=sizeof(*ptr)*3;
-    /* New version of active_size, if fail, version above is good */
-    active_size-=sizeof(*ptr)*3;
-// metadata *metadata1 = ((metadata*) ptr-1);	
-// active_size-=metadata1->block_size;
-    /* Just like in realloc, I checked size_allocation to know
-	how much mem to remove from active_size */
-    
-    	//size_t asize = (*(metadata*) ptr).block_size;
-	//size_t asize = (*(metadata*) ptr).block_size; active_size-=asize;
+    metadata *main_metadata = (metadata*) ptr;
+  
+    /* updating statistics */
     nactive--;
-    base_free(ptr);
+    active_size-=main_metadata->block_size;
+    /* initializing metadata */
+    main_metadata->file=file; 
+    main_metadata->line=line;
+    base_free((metadata*) ptr);
 }
 
 
